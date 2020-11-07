4c08739bfade410ed356d7b53e6b97623b89237f
masnurrm <nurmuhammad448@gmail.com>
2020-11-07 08:44:12 +0700

    Create Binus-Merge.cpp

---
+++ Binus-Merge.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    a=a%10;
+    b=b/10;
+    cout<<a<<b<<endl;
+    return 0;
+}
