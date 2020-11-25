a35fd6a7d0f12743a9f1955229269ed020921318
masnurrm <nurmuhammad448@gmail.com>
2020-11-25 12:22:00 +0700

    Update HR-AngryProfessor.cpp

--- HR-AngryProfessor.cpp
+++ HR-AngryProfessor.cpp
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(){
-    int t, n, k, arr[201], hadir;
+    int t, n, k, hadir;
     cin>>t;
-    for(int i=0; i<t; i++){
-        cin>>n>>k;
-        hadir=0;
-        for(int j=0; j<n; j++){
-            cin>>arr[j];
-            if (arr[j]<=0){
-                hadir=hadir+1;
+    if(t>=1&&t<=10){
+        for(int i=0; i<t; i++){
+            cin>>n>>k;
+            int arr[n];
+            if(n>=1&&n<=1000){
+                if(k>=1&&k<=n){
+                    hadir=0;
+                    for(int j=1; j<=n; j++){
+                        cin>>arr[j];
+                        if(arr[j]>=-100&&arr[j]<=100){
+                            if (arr[j]<=0){
+                                hadir=hadir+1;
+                            }
+                        }
+                    }
+                    if(hadir<k){
+                        cout<<"YES"<<endl;
+                    }
+                    else{
+                        cout<<"NO"<<endl;
+                    }
+                }
             }
         }
-        if(hadir<k){
-            cout<<"YES"<<endl;
-        }
-        else{
-            cout<<"NO"<<endl;
-        }
     }
-
     return 0;
 }
