946236d526b9b925c00de59827705ce645b5a50b
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2021-01-17 14:49:22 +0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
-#include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fs first
+#define sz size()
+#define sc second
+#define mp make_pair
 
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 998244353;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+using namespace std;
 int main(){
 
 
