57c12c54994fe1f7a2cf3f2986aad8234d9dcfab
masnurrm <sangmastahnm@gmail.com>
2025-01-22 11:39:16 +0700

    feat: leetcode 1765 Map of Highest Peak (unoptimized yet)

---
+++ leetcode/1765/ver1.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        queue<pair<int, int>> q;
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isWater[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            
+            for (auto [dx, dy] : directions) {
+                int newX = x + dx;
+                int newY = y + dy;
+                
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
+                    if (dist[newX][newY] > dist[x][y] + 1) {
+                        dist[newX][newY] = dist[x][y] + 1;
+                        q.push({newX, newY});
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+};
