7cb8a94b6a6553f52dcea868d97029609abdc518
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2025-03-13 23:24:48 +0700

    feat: Leetcode 2529. Maximum Count of Positive Integer and Negative Integer (0 ms with hint's way)

---
+++ leetcode/2529/ver2.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int negCount = lower_bound(nums.begin(), nums.end(), 0) - nums.begin();
+        int posCount = nums.end() - upper_bound(nums.begin(), nums.end(), 0);
+        
+        return max(negCount, posCount);
+    }
+};
