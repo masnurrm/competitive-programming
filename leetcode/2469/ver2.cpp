3c866c948897f975fd5a325bf26d963edc79d174
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2025-03-31 20:53:36 +0700

    Create ver2.cpp

---
+++ leetcode/2469/ver2.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> res = {celsius + 273.15, celsius * 1.80 + 32.00};
+        return res;
+    }
+};
